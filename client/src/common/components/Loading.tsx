acbe12d8ed8c5b742c1e6ffc5d6f33f34c0a5815
jk <jkwak1635@gmail.com>
2024-03-13 13:22:10 +0900

    feat: webview test

---
+++ client/src/common/components/Loading.tsx
@@ -0,0 +1,11 @@
+import BasicContainer from './BasicContainer.tsx';
+
+const Loading = () => {
+    return (
+        <BasicContainer>
+            <h1>Loading</h1>
+        </BasicContainer>
+    );
+};
+
+export default Loading;
